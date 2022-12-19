c55d387d5ea0efd3ced0a7bd6a67e05a353ecf4c
EFSilva22 <esromferreira4@gmail.com>
2022-12-19 00:00:45 -0300

    commiti-7

--- src/main/java/queimando/iftm/controller/CadastrarController.java
+++ src/main/java/queimando/iftm/controller/CadastrarController.java
@@ -1,30 +1,13 @@
 package queimando.iftm.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import queimando.iftm.model.Cadastrar;
-import queimando.iftm.repository.CadastrarRepository;
 
 @Controller
 public class CadastrarController {
 
-    @Autowired
-    CadastrarRepository repository;
-
-    @GetMapping("cad_promo")
-    public String formCadastroPromo(Model model) {
-        model.addAttribute("promocao", new Cadastrar());
-        return "form_cadastro";
+    @GetMapping("cadastrar")
+    public String cadastrar() {
+        return "cadastrarpro";
     }
-
-    @PostMapping("cad_promo")
-    public String gravaNovaPromocao(Cadastrar cadastrar, Model model) {
-
-        return "redirect:/cadastrarpro";
-    }
-
 }
