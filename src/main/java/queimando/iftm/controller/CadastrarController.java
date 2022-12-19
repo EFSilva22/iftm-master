a4c79fa14e03f78a3e0cff52b742e9c2d44065d1
EFSilva22 <esromferreira4@gmail.com>
2022-12-18 23:45:45 -0300

    commit-5

--- src/main/java/queimando/iftm/controller/CadastrarController.java
+++ src/main/java/queimando/iftm/controller/CadastrarController.java
@@ -1,22 +1,12 @@
 package queimando.iftm.controller;
 
-import java.sql.Timestamp;
-import java.util.List;
-
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.professorangoti.condominio.model.Apartamento;
-import com.professorangoti.condominio.repository.ApartamentoRepository;
-import com.professorangoti.condominio.repository.ProprietarioRepository;
-
-import queimando.iftm.model.cadastrarpro;
+import queimando.iftm.model.Cadastrar;
 import queimando.iftm.repository.CadastrarRepository;
 
 @Controller
@@ -27,29 +17,14 @@ public class CadastrarController {
 
     @GetMapping("cad_promo")
     public String formCadastroPromo(Model model) {
-        model.addAttribute("promocao", new CadastrarPro());
+        model.addAttribute("promocao", new Cadastrar());
         return "form_cadastro";
     }
 
     @PostMapping("cad_promo")
-    public String Promocao(Promocao professor) {
-        System.out.println("passei por aqui");
-        db.update("insert into professores(nome) values(?)", professor.getNome());
-        return "home";
-    }
+    public String gravaNovaPromocao(Cadastrar cadastrar, Model model) {
 
-    @GetMapping("rel_apto")
-    public String relatorio(Model model) {
-        Timestamp timestamp1 = new Timestamp(System.currentTimeMillis());
-        List<Apartamento> lista = repository.findAll();
-        Timestamp timestamp2 = new Timestamp(System.currentTimeMillis());
-        System.out.println("------------ tempo de execução: " + (timestamp2.getTime() - timestamp1.getTime()));
-        timestamp1 = new Timestamp(System.currentTimeMillis());
-        List<Apartamento> lista2 = repository.findAll2();
-        timestamp2 = new Timestamp(System.currentTimeMillis());
-        System.out.println("=============tempo de execução: " + (timestamp2.getTime() - timestamp1.getTime()));
-        model.addAttribute("apartamentos", lista);
-        return "rel_apto";
+        return "redirect:/cadastrarpro";
     }
 
 }
--- src/main/java/queimando/iftm/model/Cadastrar.java
+++ src/main/java/queimando/iftm/model/Cadastrar.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CadastrarPro {
+public class Cadastrar {
     private int id;
 
     private String nomep;
